a2426e487f20f539d721f45659577970784c307f
“youxin <youxpower@163.com>
2021-04-07 21:23:17 +0800

    基础架子上面没有任何东西  可以进行集成

---
+++ common/src/main/java/com/common/entity/User.java
@@ -0,0 +1,17 @@
+package com.common.entity;
+
+
+import lombok.Data;
+
+/**
+ * @author xin.you
+ * @date 2021/4/5
+ * @description
+ */
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private String address;
+}
---
+++ producer/src/main/java/com/producer/producer/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.producer.producer.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xin.you
+ * @date 2021/4/5
+ * @description
+ */
+@RestController
+public class TestController {
+    public static void main(String[] args) {
+
+    }
+}
